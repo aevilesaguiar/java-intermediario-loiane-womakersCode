db1b409f021aae4f6e4754035edac8f84daf609f
aevilesaguiar <aeviles@hotmail.com>
2022-01-29 22:25:52 -0300

    ffgf

--- src/Teste.java
+++ src/Teste.java
@@ -3,7 +3,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("teste");
+		System.out.println("teste dfdfdf");
 	}
 
 }
